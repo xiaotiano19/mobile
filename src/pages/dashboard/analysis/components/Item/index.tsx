5e435adcad3b539959421589694b4781b9309695
tianqing <1499762912@qq.com>
2023-04-04 13:34:59 +0800

    获取位置报错

--- src/pages/dashboard/analysis/components/Item/index.tsx
+++ src/pages/dashboard/analysis/components/Item/index.tsx
@@ -1,14 +1,25 @@
-import React from 'react'
+import React,{useRef} from 'react'
 import styles from './index.less'
-const Item = (props) => {
-    let {chat} =props;
+const Item = (props:any) => {
+    const textRef = useRef(null);
+    let {chat,setText,handleTextRef,showCursor,pos} =props;
+    const handleTextChange = (event:any) => {
+      setText(event.target.innerText);
+    };
+  
+    // 将DOM元素的引用通过props传递给父组件
+    if (handleTextRef) {
+      handleTextRef(textRef.current);
+    }
+ 
     return (
         <li className={chat.role==='user'?styles.userChatWrapper:styles.assistantChatWrapper}>
           <div className={styles.divWrapper}>
             <div className={styles.leftDiv}>
-              <img style={{width:'32.5px',height:'32.5px'}} src={chat.role==='user'?'/images/01.jpg':'/images/02.jpeg'} />
+              <img className={styles.imgContent} src={chat.role==='user'?'/images/01.jpg':'/images/02.jpeg'} />
             </div>
-            <div className={styles.rightDiv}>{chat.value}</div>
+            <div className={styles.rightDiv} ref={chat.role==='assistant'?textRef:null} onChange={chat.role==='assistant'?handleTextChange:undefined} >{chat.value}</div>
+            {showCursor?<div className={styles.cursor} style={{left:`calc(${pos.x}*1)`,top:`calc(${pos.y}*1)`}}></div>:null}
           </div>
         </li>
       );
--- src/pages/dashboard/analysis/index.jsx
+++ src/pages/dashboard/analysis/index.jsx
@@ -38,6 +38,27 @@ const App = (props) => {
  const [chatTitle, setchatTitle] = useState('')//每个聊天窗口的标题
  const [isDisabled, setisDisabled] = useState(false)
  const textAreaRef = useRef(null);
+ const [text, setText] = useState('');//回复文本
+ const [textRef, setTextRef] = useState(null);//显示回复文本的dom对象
+ const [showCursor, setshowCursor] = useState(true)
+ const [pos, setpos] = useState({x:0,y:0})
+
+    // useEffect(() => {
+    //     if (textRef) {
+    //       const textElement = textRef;
+    //       const cursorElement =cursorRef;
+    //       const textWidth = textElement.offsetWidth;
+    //       const textHeight = textElement.offsetHeight;
+    //       const cursorWidth = cursorElement.offsetWidth;
+    //       const cursorHeight = cursorElement.offsetHeight;
+    //       const cursorX = textWidth + cursorWidth / 2;
+    //       const cursorY = textHeight + cursorHeight / 2;
+    //     }
+    //   }, [text]);
+    const handleTextRef = (ref) => {
+        setTextRef(ref);
+    };
+
     const handleKeyDown = (e) => {
       if (e.keyCode === 13 && !e.shiftKey) { // 判断按下的是否为回车键
         e.preventDefault(); // 阻止默认的回车键行为
@@ -91,7 +112,8 @@ const App = (props) => {
           console.error('Error reading stream:', error);
       } finally {
           reader.releaseLock();
-          setisDisabled(false)
+          setshowCursor(false);
+          setisDisabled(false);
       }
       
     }
@@ -109,6 +131,81 @@ const App = (props) => {
       chatList.push(chat);
       setchatList([...chatList])
     }
+    // const updateCursor= ()=>{
+    //     const lastText=getLastTextNode(textRef);
+    //     const textNode=document.createTextNode('\u200b');
+    //     console.log('textNode', textNode);
+
+    //     if(lastText){
+    //         lastText.parentElement.appendChild(textNode);
+    //     }else{
+    //         textRef?.appendChild(textNode);
+    //     }
+        
+    //     const range=document.createRange();
+    //     range.setStart(textNode,0);
+    //     range.setEnd(textNode,0)
+    //     const rect = range.getBoundingClientRect();
+    //     console.log(rect.left,'rect')
+    //     const domRect=textRef?.getBoundingClientRect();
+    //     console.log(domRect)
+    //     pos.x=rect?.left-domRect.left;
+    //     pos.y=rect?.top-domRect.top;
+    //     // setTimeout(() => {
+           
+    //     // }, 0);
+     
+    //     setpos(...pos)
+    //     textNode.remove();
+    //         // rest of the code that uses `rect`
+
+    // }
+    const updateCursor= ()=>{
+        const lastText=getLastTextNode(textRef);
+        const textNode=document.createTextNode('\u200b');
+        console.log('textNode', textNode);
+    
+        if(lastText){
+            lastText.parentElement.appendChild(textNode);
+        }else{
+            textRef?.appendChild(textNode);
+        }
+    
+        const range=document.createRange();
+        range.setStart(textNode,0);
+        range.setEnd(textNode,0);
+    
+        setTimeout(() => {
+            const rect = range.getBoundingClientRect();
+            console.log(rect.left,'rect')
+            const domRect=textRef?.getBoundingClientRect();
+            console.log(domRect)
+            pos.x=rect?.left-domRect.left;
+            pos.y=rect?.top-domRect.top;
+            setpos(...pos)
+        }, 500);
+    
+        textNode.remove();
+    }
+    useEffect(() => {
+        updateCursor();
+    })
+    const getLastTextNode=(dom)=>{
+        const children=dom?.childNodes;
+        for(let i=children?.length-1;i>=0;i--){
+            const node=children[i];
+            if(node.nodeType===Node.TEXT_NODE){
+                // node.nodeValue=node.nodeValule?.replace(/\s+$/,'');
+                return node;
+            }else if(node.nodeType===Node.ELEMENT_NODE){
+                const last=getLastTextNode(node);
+                if(last){
+                    return last;
+                }
+            }
+        }
+        return null;
+    }
     const [form] = Form.useForm();
     return (
         <Space direction="vertical" style={{ width: '100%' }} size={[0, 48]}>
@@ -118,7 +215,9 @@ const App = (props) => {
                 <Content style={contentStyle}>
                   {
                   chatList?.map((chat,index)=>{
-                      return <Item key={index} chat={chat}/>
+                      return <Item key={index} chat={chat} setText={setText} 
+                      handleTextRef={handleTextRef} 
+                      showCursor={showCursor} pos={pos}/> 
                   })
                   }
                 </Content>
